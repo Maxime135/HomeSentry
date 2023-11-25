da7ca46178724adb1dd0e8c149cace5d401e46de
Maxime Boulanger <maximeboulanger_@hotmail.fr>
2023-11-25 17:42:40 +0100

    added a secrets.h template file

---
+++ Arduino/readField/secrets_template.h
@@ -0,0 +1,8 @@
+// This is a template. Fill the defferent fields and rename the file as "secrets.h" to use it.
+
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+#define SECRET_CH_ID xxxxxxx
+#define SECRET_WRITE_APIKEY ""
+#define SECRET_READ_APIKEY ""
